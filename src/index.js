842f73f9449b6cb38717f329219af74d51fb01c7
laegel <laegel@selper.co.kr>
2023-03-03 17:54:00 +0900

    Title : UseReducer 실습

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Context from "./Context";
+import Context from "./contextSample/Context";
+import UseState from "./useReducerSample/UseState";
+import UseReducer from "./useReducerSample/UseReducer";
+import Extension from "./useReducerSample/Extension";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-    <Context/>
+    {/*<Context/>*/}
+    {/*<UseState/>*/}
+    {/*<UseReducer/>*/}
+    <Extension/>
   </React.StrictMode>
 );
 
